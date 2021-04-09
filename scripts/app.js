aab652b678c07c1fb799c279228748e0d75e4f6b
nayyyhaa <nehaguptang.ng@gmail.com>
2021-04-09 17:02:18 +0530

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+// read input
+let inputElement = document.querySelector("#inputTxt");
+
+// button click
+let translateBtnElement = document.querySelector("#translateButton");
+
+// show output
+let outputElement = document.querySelector("#outputTxt");
+
+let url="https://shakespeare.p.mashape.com/shakespeare.json";
+
+function translatedURL(inputValue){
+    var encodedURI = encodeURI(inputValue);
+    let translatedURL = `${url}?text=${encodedURI}`;
+    return translatedURL;   
+}
+
+function translateFunction(event){
+    let inputValue = inputElement.value;    
+    let finalURL = translatedURL(inputValue);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            outputElement.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Something wrong with the server! Try after sometime"))
+}
+translateBtnElement.addEventListener("click", translateFunction);
