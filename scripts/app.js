99316fc9d9f86bfedfad98a2836b656e530c3268
nayyyhaa <nehaguptang.ng@gmail.com>
2021-04-11 10:19:16 +0530

    feat: prompt message

--- scripts/app.js
+++ scripts/app.js
@@ -23,6 +23,6 @@ function translateFunction(event){
         .then(json => {
             outputElement.innerText = json.contents.translated;
         })
-        .catch(() => alert("Something wrong with the server! Try after sometime"))
+        .catch(() => alert("Shakespeare(Server) is busy! Try after sometime"))
 }
 translateBtnElement.addEventListener("click", translateFunction);
